ae724e20b914225c42b5082e5260c6d8ae57d776
Melmoth <marcos.mon.rod@gmail.com>
2022-04-20 19:02:17 +0200

    fix: edit app and index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,58 +1,12 @@
 import '../styles/globals.css'
 
-import {
-  createContext,
-  useEffect,
-  useState,
-} from 'react'
-
-import { useCycle } from 'framer-motion'
-
-import { searchTeamList } from './api'
-
-export const AppContext = createContext(null)
+import { AppContextProvider } from '../Context/AppContext'
 
 function MyApp({ Component, pageProps }) {
-  const [teamList, setTeamList] = useState([])
-  const [team, setTeam] = useState()
-  const [navOpen, toggleNavOpen] = useCycle(false, true);
-  const [loading, toggleLoading] = useCycle(false, true);
-  const [modal, toggleModal] = useCycle(false, true);
-  const [players, setPlayers] = useState([])
-  const [myTeam, setMyTeam] = useState([])
-
-  const searchTeams = async () => {
-    const result = await searchTeamList()
-    setTeamList(result.teams)
-  }
-  const addToMyTeam = (playerSelected, teamSelected) => {
-    setMyTeam([...myTeam, { playerSelected, teamSelected }])
-  }
-  const deleteFromMyTeam = (playerSelected) => {
-    myTeam.filter((v) => v.id !== playerSelected.id)
-  }
-  useEffect(() => {
-    searchTeams()
-  }, [])
   return (
-    <AppContext.Provider value={{
-      teamList,
-      team,
-      setTeam,
-      navOpen,
-      toggleNavOpen,
-      loading,
-      toggleLoading,
-      modal,
-      toggleModal,
-      players,
-      setPlayers,
-      myTeam,
-      addToMyTeam,
-      deleteFromMyTeam
-    }}>
+    <AppContextProvider>
       <Component {...pageProps} />
-    </AppContext.Provider>
+    </AppContextProvider>
   )
 }
 
